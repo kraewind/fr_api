8d6d4223cf6c6fdb152f43de2ee41b24c80bd645
krae <kraewind@gmail.com>
2021-09-07 11:53:51 -0400

    Done

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -36,13 +36,8 @@ class TransactionsController < ApplicationController
       count = 0
   
       @usedTransactionsForThisSpend = []
-
-      # @firstAndLastTransactionForThisSpendAmountSpent = []
       
       until (count >= transaction_params[:points]) || (i == @sortedDateTransactions.length) do
-        # if i == 0 || i == (@sortedDateTransactions.length - 1)
-        #   @firstAndLastTransactionForThisSpendAmountSpent.push()
-        # end
         count += @sortedDateTransactions[i].remaining_balance
         @sortedDateTransactions[i].update(remaining_balance: 0, used: true, previous_remaining_value: @sortedDateTransactions[i].remaining_balance)
         @usedTransactionsForThisSpend.push(@sortedDateTransactions[i])
@@ -52,12 +47,14 @@ class TransactionsController < ApplicationController
       @hashToConsolidate = {}
   
       if @oldestRemainingTransaction && @oldestRemainingTransaction.update(remaining_balance: (count - transaction_params[:points]), used: (count - transaction_params[:points] == 0))
+        # amount = 0
         @usedTransactionsForThisSpend.each do |transaction|
-          if transaction.previous_remaining_value >= 0
-            transaction.payer.update(points: (transaction.payer.points + (transaction.remaining_balance - transaction.previous_remaining_value)))
-          else
-            transaction.payer.update(points: (transaction.payer.points + transaction.previous_remaining_value))
-          end
+          # amount += transaction.previous_remaining_value - transaction.remaining_balance
+          # if transaction.previous_remaining_value >= 0
+          #   transaction.payer.update(points: (transaction.payer.points + (transaction.remaining_balance - transaction.previous_remaining_value)))
+          # else
+          #   transaction.payer.update(points: (transaction.payer.points + transaction.previous_remaining_value))
+          # end
           if @hashToConsolidate.keys.include?(transaction.payer.name)
             @hashToConsolidate[transaction.payer.name] += (transaction.remaining_balance - transaction.previous_remaining_value)
           else
@@ -66,23 +63,12 @@ class TransactionsController < ApplicationController
         end
         arrayToBeReturned = []
 
-        @hashToConsolidate.each {|k, v| arrayToBeReturned.push({"payer": k, "points": v})}
-      render json: arrayToBeReturned
-        
-        # consolidatedArray = []
-        # @arrayBeforeConsolidated.each |hashh| do 
-        #   existing = @arrayBeforeConsolidated.select {|t| t["payer"] == hashh["payer"]}
-        #   if (existing.length > 0)
-        #     points = 0
-        #     existing.each |e| do
-        #       points += e["points"]
-        #     end 
-        #     # existingIndex = consolidatedArray.index(existing[0])
-        #     # consolidatedArray[existingIndex]["points"] += hashh["points"]
-        #   else
-        #     consolidatedArray.push(hashh)
-        #   end
-        # end
+        @hashToConsolidate.each do |key, value|
+          focus = Payer.find_by(name: key)
+          focus.update(points: (focus.points + value))
+          arrayToBeReturned.push({"payer": key, "points": value})
+        end
+        render json: arrayToBeReturned
       else
         render json: @oldestRemainingTransaction.errors, status: :unprocessable_entity
       end
